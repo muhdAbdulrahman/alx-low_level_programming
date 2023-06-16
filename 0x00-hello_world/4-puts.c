2ce9eec1633440010880475f81bd20cfcab54f2e
Muhammad Abdulrahman <mabdul.am25@gmail.com>
2023-06-15 17:46:16 -0700

    -4-puts.c added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main -Program that println using puts fnc
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
