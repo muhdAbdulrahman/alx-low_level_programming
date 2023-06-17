bd705ccb45b26f833381bf03715f0d931d719532
Muhammad Abdulrahman <mabdul.am25@gmail.com>
2023-06-17 15:03:31 -0700

    6-print_numberz.c added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int first = 0, last = 9;
+
+	while (first <= last)
+	{
+		/**
+		 *this code convert first to char and
+		 *convert to number printable with putchar
+		 */
+		putchar((char)first + '0');
+		start++;
+	}
+	putchar('\n');
+	return (0);
+}
