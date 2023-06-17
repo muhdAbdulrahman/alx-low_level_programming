b61681c8bc785a33368caecad40f711b741a68ce
Muhammad Abdulrahman <mabdul.am25@gmail.com>
2023-06-17 16:20:53 -0700

    101-print_comb.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,22 +5,22 @@
  */
 int main(void)
 {
-	int i = 0, j = 0, k = 0;
+	int i = 0, j = 1, k = 2;
 
-	while (i < 10)
+	while (i < 8)
 	{
 		j = i;
-		while (j < 10)
+		while (j < 9)
 		{
 			k = j;
-			while (k < 0)
+			while (k < 10)
 			{
-				if (i != j && j != k)
+				if (i != j && i != k && j != k)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-					if ((i != 8) || (j != 9) || (k != 9))
+					if ((i != 7) || (j != 8) || (k != 9))
 					{
 						putchar(',');
 						putchar(' ');
@@ -29,8 +29,11 @@ int main(void)
 				k++;
 			}
 			j++;
+			k = j + 1;
 		}
 		i++;
+		j = i + 1;
+		k = j + 1;
 	}
 	putchar('\n');
 	return (0);
