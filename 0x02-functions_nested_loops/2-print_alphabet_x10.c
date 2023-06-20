5b4dc6315034a545e4a4b9126577d6879e72a482
Muhammad Abdulrahman <mabdul.am25@gmail.com>
2023-06-20 15:51:07 -0700

    2-print_alphabet_10X

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a';
+
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(letter);
+			letter++;
+		}
+
+		_putchar('\n');
+	}
+}
